d9375a0ff5b67d9458eb7b8e0aee80c2dd356440
Jamie Mahowald <j.mahowald@utexas.edu>
2024-06-21 14:49:28 -0500

    added 3D Gaussian process function

--- icon-lm/data_preparation/data_utils.py
+++ icon-lm/data_preparation/data_utils.py
@@ -42,6 +42,10 @@ def rbf_circle_kernel_jax(x1, x2, sigma, l):
     sq_norm = (xx1_1-xx2_1)**2/(l**2) + (xx1_2-xx2_2)**2/(l**2)
     return sigma**2 * jnp.exp(-0.5 * sq_norm)
 
+def rbf_kernel_3d(X1, X2, k_sigma, k_l):
+    sqdist = jnp.sum((X1[:, None, :] - X2[None, :, :]) ** 2, axis=-1)
+    return k_sigma * jnp.exp(-0.5 / k_l**2 * sqdist)
+
 @partial(jax.jit, static_argnames=('num','kernel'))
 def generate_gaussian_process(key, ts, num, kernel, k_sigma, k_l):
   '''
@@ -56,4 +60,17 @@ def generate_gaussian_process(key, ts, num, kernel, k_sigma, k_l):
   out = jax.random.multivariate_normal(key, mean=mean, cov=cov, shape=(num,), method='svd')
   return out
 
-
+def generate_gaussian_process_3d(key, ts, xs, num, kernel, k_sigma, k_l):
+    '''
+    ts: 1D array (length,)
+    out: Gaussian process samples, 2D array (num, length)
+    '''
+    X, T = jnp.meshgrid(xs, ts)
+    points = jnp.vstack([X.ravel(), T.ravel()]).T
+    cov = kernel(points, points, k_sigma=k_sigma, k_l=k_l)
+    cov += 1e-6 * jnp.eye(cov.shape[0]) # Add a small jitter for numerical stability
+    mean = jnp.zeros(cov.shape[0])
+    cov = einshape('ii->nii', cov, n=num)
+    u_sample = jax.random.multivariate_normal(key, mean=mean, cov=cov, shape=(num,), method='svd')
+    out = u_sample.reshape((num, X.shape[0], X.shape[1]))
+    return out

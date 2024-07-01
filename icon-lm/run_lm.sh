ac71ae38c575e04582f6c73805db8cf13d4e9e64
Jamie Mahowald <j.mahowald@utexas.edu>
2024-07-01 11:27:21 -0500

    shell to run only single-modal icon-LM

---
+++ icon-lm/run_lm.sh
@@ -0,0 +1,9 @@
+# ICON-LM (Single-Modal):
+CUDA_VISIBLE_DEVICES=0 python3 run.py --problem 'icon_lm' --epochs 100 \
+  --train_batch_size 24 --train_data_dirs '/workspace/Jamie/in-context-operator-networks/icon-lm/data/data0910c' \
+  --model_config_filename 'model_lm_config.json' \
+  --train_config_filename 'train_lm_config.json' \
+  --test_config_filename 'test_lm_config.json' \
+  --train_data_globs 'train*' --test_data_globs 'test*' \
+  --test_demo_num_list 1,3,5 --model icon_lm --loss_mode nocap \
+  --nodeterministic --seed 1 --vistest --tfboard 
